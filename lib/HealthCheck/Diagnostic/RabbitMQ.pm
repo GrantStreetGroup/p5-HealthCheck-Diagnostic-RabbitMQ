6d5a808d988623426b9a22e7cc6c2917b79e52a0
Andrew Fresh <andrew.fresh@grantstreet.com>
2018-08-21 20:27:24 -0400

    Allow checking that queues exist
    This requires that you open a channel and tell us what it is and the
    name of the queue to check.

--- lib/HealthCheck/Diagnostic/RabbitMQ.pm
+++ lib/HealthCheck/Diagnostic/RabbitMQ.pm
@@ -26,16 +26,58 @@ sub new {
 sub check {
     my ( $self, %params ) = @_;
 
-    my $rabbit_mq = $params{rabbit_mq};
-    $rabbit_mq ||= $self->{rabbit_mq} if ref $self;
-    $rabbit_mq = $rabbit_mq->(%params) if ref $rabbit_mq eq 'CODE';
-
+    # The method the object needs to have for us to proceed
     my $method = 'get_server_properties';
+
+    # These are the params that we actually use to make our decisions
+    # and that we're going to return in the result to make that clear.
+    my %decision_params = ( rabbit_mq => undef );
+
+    # If we have a queue to check, that changes our requirements
+    if ( defined $params{queue}
+        or ( ref $self and defined $self->{queue} ) )
+    {
+        $method = 'queue_declare';
+        $decision_params{$_} = undef for qw(
+            queue
+            channel
+        );
+    }
+
+    # Now we prefer the params passed to check,
+    # and fall back to what is on the instance.
+    foreach my $param ( keys %decision_params ) {
+        $decision_params{$param} = $params{$param};
+        $decision_params{$param} ||= $self->{$param} if ref $self;
+    }
+
+    # The rabbit_mq param was only "known" so we could choose between
+    # one that was passed to check and the one on the instance.
+    my $rabbit_mq = delete $decision_params{rabbit_mq};
+    $rabbit_mq = $rabbit_mq->(%params) if ref $rabbit_mq eq 'CODE';
 
     croak("'rabbit_mq' must have '$method' method") unless $rabbit_mq and do {
         local $@; eval { local $SIG{__DIE__}; $rabbit_mq->can($method) } };
 
-    my $res = $self->SUPER::check( %params, rabbit_mq => $rabbit_mq );
+    # In theory we could default to random channel in the
+    # range of 1..$rabbit_mq->get_max_channel
+    # but then we would have to:
+    # 1. Hope it's not in use
+    # 2. Open and then close it.
+    # Instead we default to 1 as that's what our internal code does.
+    $decision_params{channel} //= 1
+        if exists $decision_params{channel};
+
+    my $res = $self->SUPER::check(
+        %params,
+        %decision_params,
+        rabbit_mq => $rabbit_mq,
+    );
+
+    # Make sure we report what we actually *used*
+    # not what our parent may have copied out of %{ $self }
+    $res->{data} = { %{ $res->{data} || {} }, %decision_params }
+        if %decision_params;
     delete $res->{rabbit_mq};    # don't include the object in the result
 
     return $res;
@@ -45,15 +87,29 @@ sub run {
     my ( $self, %params ) = @_;
     my $rabbit_mq = $params{rabbit_mq};
 
+    my $cb = sub { $rabbit_mq->get_server_properties };
+
+    if ( defined $params{queue} ) {
+        my $queue   = $params{queue};
+        my $channel = $params{channel};
+
+        $cb = sub {
+            my $name = $rabbit_mq->queue_declare( $channel, $queue,
+                { passive => 1 } );
+
+            return { name => $name };
+        };
+    }
+
     my $data;
     {
         local $@;
-        $data
-            = eval { local $SIG{__DIE__}; $rabbit_mq->get_server_properties };
+        $data = eval { local $SIG{__DIE__}; $cb->() };
 
         if ( my $e = $@ ) {
             my $file = quotemeta __FILE__;
             $e =~ s/ at $file line \d+\.?\n\Z//ms;
+            $e =~ s/^Declaring queue: //;
             return { status => 'CRITICAL', info => $e };
         }
     }
@@ -66,15 +122,57 @@ __END__
 
 =head1 SYNOPSIS
 
+Check that you can talk to the server.
+
     my $health_check = HealthCheck->new( checks => [
         HealthCheck::Diagnostic::RabbitMQ->new( rabbit_mq => \&connect_mq ),
     ] );
+
+Or verify that a queue exists,
+has an appropriate number of listeners,
+and not too many queued messages waiting.
+
+    my $check_rabbit_mq => HealthCheck::Diagnostic::RabbitMQ->new(
+        rabbit_mq => \&connect_mq,
+        queue     => $queue_name,
+        channel   => $channel,       # default channel is 1
+    );
+
+    my $health_check = HealthCheck->new( checks => [$check_rabbit_mq] );
+
+Here the C<connect_mq> function could be something like:
+
+    sub connect_mq {
+        my $mq = Net::AMQP::RabbitMQ->new;
+        $mq->connect( $host, {
+            user            => $username,
+            password        => $password,
+            vhost           => $vhost,
+        } );
+        $mq->channel_open(1);
+        return $mq;
+    };
+
+The C<< $mq->channel_open >> is only needed to check a queue,
+in which case you will need to open the L</channel> that will be used.
+
+Checking additional queues could be as easy as:
+
+    $health_check->register( {
+        label    => "other_rabbit_mq_check",
+        invocant => $check_rabbit_mq,
+        check    => sub { shift->check( @_, queue => 'other.queue' },
+    } );
+
 
 =head1 DESCRIPTION
 
 Determines if the RabbitMQ connection is available.
 Sets the C<status> to "OK" or "CRITICAL" based on the
 return value from C<< rabbit_mq->get_server_properties >>.
+
+If you pass in a L</queue>,
+it will instead check that the queue exists.
 
 =head1 ATTRIBUTES
 
@@ -83,10 +181,31 @@ Can be passed either to C<new> or C<check>.
 =head2 rabbit_mq
 
 A coderef that returns a
-L<Net::AMQP::RabbitMQ> or compatible object,
+L<Net::AMQP::RabbitMQ> or L<Net::RabbitMQ> or compatible object,
 or the object itself.
 
-=head2
+=head2 queue
+
+The name of the queue to check whether it exists.
+
+Accomplishes the check by using C<< rabbit_mq->queue_declare >>
+to try to declare a passive queue.
+Requires a L</channel>.
+
+=head2 channel
+
+Allow specifying which channel will be used to check the L</queue>.
+
+The passed in L</rabbit_mq> must open this channel with C<channel_open>
+to use this method.
+
+Defaults to 1.
+
+=head1 BUGS AND LIMITATIONS
+
+L<Net::RabbitMQ> does not support C<get_server_properties> and so doesn't
+provide a way to just check that the server is responding to
+requests.
 
 =head1 DEPENDENCIES
 
--- t/HealthCheck-Diagnostic-RabbitMQ.t
+++ t/HealthCheck-Diagnostic-RabbitMQ.t
@@ -24,8 +24,25 @@ is $@, sprintf( $must_have, __FILE__, __LINE__ - 1 ), "rabbit_mq as anonymous ob
 eval { HealthCheck::Diagnostic::RabbitMQ->check( rabbit_mq => sub {} ) };
 is $@, sprintf( $must_have, __FILE__, __LINE__ - 1 ), "rabbit_mq as empty coderef";
 
+eval { HealthCheck::Diagnostic::RabbitMQ->check( queue => undef ) };
+is $@, sprintf( $must_have, __FILE__, __LINE__ - 1 ),
+    "A queue with an empty name is ignored";
 
-my $rabbit_mq = sub { return bless {}, 'My::RabbitMQ' };
+$must_have =~ s/get_server_properties/queue_declare/;
+eval { HealthCheck::Diagnostic::RabbitMQ->check( queue => 'a.queue' ) };
+is $@, sprintf( $must_have, __FILE__, __LINE__ - 1 ),
+    "The required method changes with a queue";
+
+eval { HealthCheck::Diagnostic::RabbitMQ->check( queue => 0 ) };
+is $@, sprintf( $must_have, __FILE__, __LINE__ - 1 ),
+    "An falsy queue with length (0) is understood";
+
+eval { HealthCheck::Diagnostic::RabbitMQ->check( queue => '' ) };
+is $@, sprintf( $must_have, __FILE__, __LINE__ - 1 ),
+    "An empty string queue is understood";
+
+
+my $rabbit_mq = sub { My::RabbitMQ->new };
 
 {
     no warnings 'once';
@@ -83,4 +100,148 @@ my $rabbit_mq = sub { return bless {}, 'My::RabbitMQ' };
         "CRITICAL status with stack trace as expected";
 }
 
+{
+    my $exception;
+    no warnings 'once';
+    local *My::RabbitMQ::queue_declare
+        = sub { Carp::croak("Declaring queue: $exception") };
+    use warnings 'once';
+
+    foreach (
+        q{server connection error 504, message: CHANNEL_ERROR - expected 'channel.open'},
+        q{server channel error 404, message: NOT_FOUND - no queue 'nonexist' in vhost 'test'},
+        )
+    {
+        $exception = $_;
+        is_deeply(
+            HealthCheck::Diagnostic::RabbitMQ->check(
+                rabbit_mq => $rabbit_mq,
+                queue     => 'nonexist',
+            ),
+            {   'status' => 'CRITICAL',
+                info     => $exception,
+                data     => {
+                    queue   => 'nonexist',
+                    channel => 1,
+                },
+            },
+            "CRITICAL status with expected exception"
+        );
+    }
+}
+
+{
+    my @args;
+    no warnings 'once';
+    local *My::RabbitMQ::queue_declare = sub { @args = @_; 'fake.queue' };
+    use warnings 'once';
+
+    is_deeply(
+        HealthCheck::Diagnostic::RabbitMQ->new(
+            rabbit_mq => $rabbit_mq,
+            queue     => 'a.queue',
+            )->check,
+        {   label  => 'rabbit_mq',
+            status => 'OK',
+            data   => {
+                queue   => 'a.queue',
+                channel => 1,
+                name    => 'fake.queue',
+            },
+        },
+        "OK status as expected"
+    );
+    isa_ok shift(@args), 'My::RabbitMQ';
+    is_deeply \@args, [ 1, 'a.queue', { passive => 1 } ],
+        "Object passed expected args to queue_declare";
+
+    is_deeply(
+        HealthCheck::Diagnostic::RabbitMQ->new(
+            rabbit_mq => $rabbit_mq,
+            queue     => 'a.queue',
+            channel   => 123,
+            )->check,
+        {   label  => 'rabbit_mq',
+            status => 'OK',
+            data   => {
+                queue   => 'a.queue',
+                channel => 123,
+                name    => 'fake.queue',
+            },
+        },
+        "OK status as expected"
+    );
+    isa_ok shift(@args), 'My::RabbitMQ';
+    is_deeply \@args, [ 123, 'a.queue', { passive => 1 } ],
+        "Object passed expected args to queue_declare with custom channel";
+
+    is_deeply(
+        HealthCheck::Diagnostic::RabbitMQ->check(
+            rabbit_mq => $rabbit_mq->(),
+            queue     => 'a.queue',
+        ),
+        {   status => 'OK',
+            data   => {
+                queue   => 'a.queue',
+                channel => 1,
+                name    => 'fake.queue'
+            }
+        },
+        "OK status as expected with rabbit_mq object passed to check()"
+    );
+    isa_ok shift(@args), 'My::RabbitMQ';
+    is_deeply \@args, [ 1, 'a.queue', { passive => 1 } ],
+        "Class passed expected args to queue_declare";
+
+    is_deeply(
+        HealthCheck::Diagnostic::RabbitMQ->check(
+            rabbit_mq => $rabbit_mq->(),
+            queue     => 'a.queue',
+            channel   => 321,
+        ),
+        {   status => 'OK',
+            data   => {
+                queue   => 'a.queue',
+                channel => 321,
+                name    => 'fake.queue',
+            }
+        },
+        "OK status as expected with rabbit_mq object passed to check()"
+    );
+    isa_ok shift(@args), 'My::RabbitMQ';
+    is_deeply \@args, [ 321, 'a.queue', { passive => 1 } ],
+        "Class passed expected args to queue_declare with custom channel";
+
+    is_deeply(
+        HealthCheck::Diagnostic::RabbitMQ->new(
+            rabbit_mq => 'ignored_mq',
+            queue     => 'ignored.queue',
+            channel   => 'ignored channel',
+            copied    => 'copied.value',
+        )->check(
+            label     => 'ignored_label',
+            rabbit_mq => $rabbit_mq->(),
+            queue     => 'a.queue',
+            channel   => 321,
+            copied    => 'passed to check',
+        ),
+        {   label => 'rabbit_mq',
+            status  => 'OK',
+            data => {
+                queue   => 'a.queue',
+                channel => 321,
+                name    => 'fake.queue',
+            },
+        },
+        "Copied somewhat expected params to result"
+    );
+    isa_ok shift(@args), 'My::RabbitMQ';
+    is_deeply \@args, [ 321, 'a.queue', { passive => 1 } ],
+        "Class passed expected args to queue_declare with custom channel";
+}
+
 done_testing;
+
+package My::RabbitMQ;
+
+sub new { return bless {}, $_[0] }
